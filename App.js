115bd520ded0498133a9cfb7832000659686ed87
Aishvarya0510 <aishvaryas0510@gmail.com>
2025-11-13 17:05:18 +0100

    Added Parcel bundler and learnings about HMR

--- App.js
+++ App.js
@@ -1,3 +1,36 @@
+/*
+npm init - create package.json,node modules
+npm init -y -> ignore questions to create project
+npm i -D parcel
+npx parcel index.html (entry of the project - file)
+parcel - created server, enabled all below
+
+
+  HMR - Hot Module Replacement
+  Internal algo of HMR -> File watcher algorithm -C++
+  Once we give ctrl + s this algo will run.
+  MINIFY
+  Cleaning our code
+  Dev and Production Build
+  Super Fast build algorithm
+  Image Optimization
+  Caching while development
+  HTTPS on dev
+  port number
+  consistent Hasing algorithm
+  zero config
+  transitive dependencies
+  -----------------------------------------------
+
+  https://browsersl.ist/
+  https://browserslist.dev/?q=bGFzdCAyIENocm9tZSB2ZXJzaW9ucw%3D%3D
+  browserslist - make this app compactible for all browsers
+*/
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
       "h1",
       {
